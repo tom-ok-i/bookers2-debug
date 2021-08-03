75ad22b0d6707435e48124624f608ad22dbbd052
EC2 Default User <ec2-user@ip-172-31-83-227.ec2.internal>
2021-08-03 04:44:44 +0000

    [finish]bookers2-debug

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -2,9 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -21,8 +24,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
+     unless @user.id == current_user.id
+       redirect_to books_path
+     end
   end
-
 
 
   def update
@@ -34,16 +40,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
-
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
